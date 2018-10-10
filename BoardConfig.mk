46b5970d4df05cd4a7bbdb71d043b809c338bf2c
TheScarastic <warabhishek@gmail.com>
2018-10-10 04:42:39 -0400

    polaris: Enable FDE encryption
    Change-Id: I248a4b74332d823fa1e4f51df0ff3f671f86bb9c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/polaris
 # Assert
 TARGET_OTA_ASSERT_DEVICE := polaris
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Kernel
 TARGET_KERNEL_CONFIG := polaris_defconfig
 
