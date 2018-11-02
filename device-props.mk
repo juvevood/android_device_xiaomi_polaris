e96d95bc5b833f691015ce4567e5dbe7e1f6c259
TheScarastic <warabhishek@gmail.com>
2018-11-02 16:17:49 -0400

    merge

--- device-props.mk
+++ device-props.mk
@@ -1,1 +1,13 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.sat.fallback.dist=45 \
+    persist.camera.sat.fallback.dist.d=5 \
+    persist.camera.sat.fallback.luxindex=405 \
+    persist.camera.sat.fallback.lux.d=20
 
+# Display features
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.displayfeature.histogram.enable=true \
+    ro.eyecare.brightness.threshold=15 \
+    ro.eyecare.brightness.level=8 \
+    ro.whitepoint_calibration_enable=false
