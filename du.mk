78e60397e9afcdd66e428c03e6f04840507c6080
juvevood <juvemail8@gmail.com>
2018-12-20 14:45:09 +0800

    init config

--- du.mk
+++ du.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosip_polaris
+PRODUCT_NAME := du_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
